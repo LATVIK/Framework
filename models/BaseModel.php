05dd6905aa89617a187ee784bc2a2e6f794de311
LATVIK <vitalyk20045@gmail.com>
2023-03-17 21:18:33 +0200

    function save() for models

--- models/BaseModel.php
+++ models/BaseModel.php
@@ -29,13 +29,76 @@ abstract class BaseModel
         return lcfirst(str_replace('_', '', ucwords($source, '_')));
     }
 
+    private function camelCaseToUnderscore(string $source): string
+    {
+        return strtolower(preg_replace('/[A-Z]/', '_$0', $source));     //source recommends  '/(?<!^)[A-Z]/'
+    }
+
+    private function mapPropertiesToDbFormat(): array
+    {
+        $reflector = new \ReflectionObject($this);
+        $properties = $reflector->getProperties();
+
+        $mappedProperties = [];
+        foreach ($properties as $property) {
+            $propertyName = $property->getName();
+            $propertyNameAsUnderS = $this->camelCaseToUnderscore($propertyName);
+            $mappedProperties[$propertyNameAsUnderS] = $this->$propertyName;
+        }
+
+        return $mappedProperties;
+    }
+
+    public function save()
+    {
+        $properties = $this->mapPropertiesToDbFormat();
+        if ($this->id !== null) {
+            $this->update($properties);
+        } else {
+            $this->insert($properties);
+        }
+    }
+
+    private function update(array $properties): void
+    {
+        $sql = 'UPDATE '
+            . static::getTableName()
+            . ' SET ' . implode('= ? , ', array_keys($properties)) . '= ?'
+            . ' WHERE id = ' . $this->id;
+        $db = DataBase::getInstance();
+        $db->query($sql, array_values($properties), static::class);
+    }
+
+    private function insert(array $properties): void
+    {
+        $properties = array_filter($properties);
+
+        $sql = 'INSERT INTO '
+            . static::getTableName()
+            . ' (' . implode(', ', array_keys($properties))
+            . ') VALUES (' . str_repeat(' ? ,', count($properties)-1) . '? );';
+
+        $db = DataBase::getInstance();
+        $db->query($sql, array_values($properties), static::class);
+    }
+
+    public function delete(): void
+    {
+        $db = DataBase::getInstance();
+        $db->query(
+            'DELETE FROM `' . static::getTableName() . '` WHERE id = :id',
+            [':id' => $this->id]
+        );
+        $this->id = null;
+    }
+
     public static function findAll(): array
     {
         $db = DataBase::getInstance();
         return $db->query('SELECT * FROM ' . static::getTableNAme() . ' ORDER by id DESC;', [], static::class);
     }
 
-    public static function findById(int $id): ?self
+    public static function findByColumn(string $columnName, $value): ?array
     {
         $db = DataBase::getInstance();
         $entities = $db->query(
